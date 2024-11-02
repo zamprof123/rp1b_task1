02e8c445f7087532aa995e3d5d8349f04992fa47
tm2463 <tm2463@bath.ac.uk>
2024-11-02 12:14:29 +0000

    Update and rename GC_content to GC_content.py

--- tom_task1/GC_content.py
+++ tom_task1/GC_content.py
@@ -1,21 +1,18 @@
-#Computing GC Content
-
-import os #import module to allow access to files on desktop
+import os #read file from desktop
 
 with open((os.path.expanduser('~/Desktop/rosalind_gc.txt')), 'r') as f:
     file = f.read().replace('\n', '').split('>')[1:]
-#create list containing file contents
 
-def get_gc_content(seq):
+
+def get_gc_content(seq): #function to compute GC content
     return ((seq.count('G') + seq.count('C'))/len(seq)) * 100
-#function to compute GC content
 
-dict = {}
+
+dict = {} #dictionary created to store identifiers and appropriate GC content
 for string in file:
     ID = string[:13]
     dict[ID] = get_gc_content(string[13:])
-#dictionary created to store identifiers and appropriate GC content
+
 
 print(max(dict, key=dict.get))
 print(max(dict.values()))
-#printing output in desired format
