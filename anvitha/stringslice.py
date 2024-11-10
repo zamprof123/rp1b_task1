7dcfc1155c37797f81a85e634d0479c0fd9603f9
ama304 <ama304@bath.ac.uk>
2024-11-10 01:15:14 +0000

    Add files via upload

---
+++ anvitha/stringslice.py
@@ -0,0 +1,6 @@
+a="WQMn8jXTHjNyi38zUL19uikFlNbBy00sqFHOTKKyW6AjpZ1tSyrrhaptesWQ7PJ5k6F7Ivh7Fp49u2gj6m5X3WnBKBdahuricaOW4JYkdwbFVn8CWfvTjZal6F8zq5OuIYo3zbrxxaH4kdqqutEUlOWWuJ9U9BvBVpfBO2pTBwkZHkpycTLj"
+
+c= a[48:57+1]+" "+a[90:97+1]
+
+print(c)
+
